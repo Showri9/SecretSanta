243d2765695ab114a06799b38128a370f68aeed2
Showri <showrirock@gmail.com>
2024-12-21 13:51:24 -0500

    Updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('src'));
+app.use(express.static(path.join(__dirname, '../Frontend')));
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
